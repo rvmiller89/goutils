35615ca7ad292b9320aba2ac5fe84cf10c152115
Ryan Miller <rvmiller89@gmail.com>
2020-06-13 20:02:38 -0700

    Implement Filter and Map

--- collection/collection.go
+++ collection/collection.go
@@ -1,7 +1,7 @@
 package collection
 
-// Index returns the first index of t in s, or -1 of it does not exist
-func Index(s []interface{}, t interface{}) int {
+// IndexOf returns the first index of t in s, or -1 of it does not exist
+func IndexOf(s []interface{}, t interface{}) int {
 	for i, v := range s {
 		if v == t {
 			return i
@@ -28,4 +28,23 @@ func Forall(s []interface{}, p func(interface{}) bool) bool {
 		}
 	}
 	return true
+}
+
+// Filter returns a slice of elements that satisfy the given predicate function
+func Filter(s []interface{}, p func(interface{}) bool) []interface{} {
+	match := make([]interface{}, 0)
+	for _, v := range s {
+		if p(v) {
+			match = append(match, v)
+		}
+	}
+	return match
+}
+
+// Map returns the slice of elements after applying the function f to each element
+func Map(s []interface{}, f func(interface{}) interface{}) []interface{} {
+	for i, v := range s {
+		s[i] = f(v)
+	}
+	return s
 }
--- collection/collection_test.go
+++ collection/collection_test.go
@@ -2,8 +2,14 @@ package collection
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 )
+
+func initEmpty() []interface{} {
+	return make([]interface{}, 0)
+}
 
 func initCollection() []interface{} {
 	var c1 = []string{"apple", "banana", "apple", "cat", "dog"}
@@ -13,13 +19,15 @@ func initCollection() []interface{} {
 	}
 	return c2
 }
-func TestIndex(t *testing.T) {
+func TestIndexOf(t *testing.T) {
+	empty := initEmpty()
 	col := initCollection()
 	tests := []struct {
 		col      []interface{}
 		target   string
 		expected int
 	}{
+		{empty, "banana", -1},
 		{col, "banana", 1},
 		{col, "apple", 0},
 		{col, "missing", -1},
@@ -30,7 +38,7 @@ func TestIndex(t *testing.T) {
 	for _, test := range tests {
 		testname := fmt.Sprintf("%v,%v,%d", test.col, test.target, test.expected)
 		t.Run(testname, func(t *testing.T) {
-			ans := Index(test.col, test.target)
+			ans := IndexOf(test.col, test.target)
 			if ans != test.expected {
 				t.Errorf("Wanted %d, Got %d", test.expected, ans)
 			}
@@ -39,12 +47,14 @@ func TestIndex(t *testing.T) {
 }
 
 func TestExists(t *testing.T) {
+	empty := initEmpty()
 	col := initCollection()
 	tests := []struct {
 		col      []interface{}
 		p        func(interface{}) bool
 		expected bool
 	}{
+		{empty, func(s interface{}) bool { return s == "kitten" }, false},
 		{col, func(s interface{}) bool { return s == "kitten" }, false},
 		{col, func(s interface{}) bool { return s == "cat" }, true},
 		{col, func(s interface{}) bool { s2, _ := s.(string); return len(s2) >= 3 }, true},
@@ -62,12 +72,14 @@ func TestExists(t *testing.T) {
 }
 
 func TestForall(t *testing.T) {
+	empty := initEmpty()
 	col := initCollection()
 	tests := []struct {
 		col      []interface{}
 		p        func(interface{}) bool
 		expected bool
 	}{
+		{empty, func(s interface{}) bool { return s == "missing" }, true},
 		{col, func(s interface{}) bool { return s == "missing" }, false},
 		{col, func(s interface{}) bool { return s == "cat" }, false},
 		{col, func(s interface{}) bool { s2, _ := s.(string); return len(s2) >= 3 }, true},
@@ -78,6 +90,57 @@ func TestForall(t *testing.T) {
 		t.Run(testname, func(t *testing.T) {
 			ans := Forall(test.col, test.p)
 			if ans != test.expected {
+				t.Errorf("Wanted %v, Got %v", test.expected, ans)
+			}
+		})
+	}
+}
+
+func TestFilter(t *testing.T) {
+	empty := initEmpty()
+	col := initCollection()
+	tests := []struct {
+		col      []interface{}
+		p        func(interface{}) bool
+		expected []interface{}
+	}{
+		{col, func(s interface{}) bool { return s == "cat" }, append(empty, "cat")},
+		{empty, func(s interface{}) bool { return s == "cat" }, empty},
+		{col, func(s interface{}) bool { s2, _ := s.(string); return strings.Contains(s2, "a") },
+			append(empty, "apple", "banana", "apple", "cat")},
+		{col, func(s interface{}) bool { s2, _ := s.(string); return len(s2) == 0 }, empty},
+	}
+
+	for i, test := range tests {
+		testname := fmt.Sprintf("Test %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := Filter(test.col, test.p)
+			if !reflect.DeepEqual(ans, test.expected) {
+				t.Errorf("Wanted %v, Got %v", test.expected, ans)
+			}
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	empty := initEmpty()
+	col := initCollection()
+	tests := []struct{
+		col []interface{}
+		f func(interface{}) interface{}
+		expected []interface{}
+	}{
+		{col, func(s interface{}) interface{} { s2, _ := s.(string); return strings.ToUpper(s2) }, 
+		append(empty, "APPLE", "BANANA", "APPLE", "CAT", "DOG")},
+		{empty, func(s interface{}) interface{} { s2, _ := s.(string); return strings.ToUpper(s2) }, empty},
+		{col, func(s interface{}) interface{} { s2, _ := s.(string); return len(s2) }, append(empty, 5, 6, 5, 3, 3)},
+	}
+
+	for i, test := range tests {
+		testname := fmt.Sprintf("Test %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := Map(test.col, test.f)
+			if !reflect.DeepEqual(ans, test.expected) {
 				t.Errorf("Wanted %v, Got %v", test.expected, ans)
 			}
 		})
